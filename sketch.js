e3e081fbfd77b7daa78dd6a4ba4ffefc06c677e3
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 16:53:14 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let imgs = [];
+let img1, img2, img3;
+
+function preload() {
+  img1 = loadImage('Images/Mario.webp');
+  img2 = loadImage('Images/Panda.jpg');
+  img3 = loadImage('Images/Racoon.jpg');
+}
+
 function setup() {
   createCanvas(400, 400);
+  imgs = [img1, img2, img3,];
+  let randomImg = random(imgs);
+  image(randomImg, 50, 50, 300, 300);
+  
+  noFill();
+  stroke(0);
+  strokeWeight(4);
+  rect(50, 50, 300, 300);
 }
 
 function draw() {
-  background(220);
 }
